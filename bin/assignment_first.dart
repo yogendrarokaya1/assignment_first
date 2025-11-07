bfde792998ceb4bb985094a0047165e7730f09d0
yogendrarokaya1 <yogendrarokaya@gmail.com>
2025-11-07 13:11:25 +0545

    first commit

---
+++ bin/assignment_first.dart
@@ -0,0 +1,5 @@
+import 'package:assignment_first/assignment_first.dart' as assignment_first;
+
+void main(List<String> arguments) {
+  print('Hello world: ${assignment_first.calculate()}!');
+}
---
+++ test/assignment_first_test.dart
@@ -0,0 +1,8 @@
+import 'package:assignment_first/assignment_first.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
